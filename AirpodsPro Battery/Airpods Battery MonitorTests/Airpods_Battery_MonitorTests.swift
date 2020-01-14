a7c137c16f8e660147997a43410fdc8f563f51bc
Mohamed Arradi-Alaoui <mohamedarradi.alaoui@dnata.com>
2020-01-14 22:52:52 +0400

    add test target

---
+++ AirpodsPro Battery/Airpods Battery MonitorTests/Airpods_Battery_MonitorTests.swift
@@ -0,0 +1,33 @@
+//
+//  Airpods_Battery_MonitorTests.swift
+//  Airpods Battery MonitorTests
+//
+//  Created by Mohamed Arradi on 14/01/2020.
+//  Copyright © 2020 Mohamed Arradi. All rights reserved.
+//
+
+import XCTest
+
+class Airpods_Battery_MonitorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
